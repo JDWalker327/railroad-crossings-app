0058687405b018267256b0abb40c7d4a6a9f832e
JDWalker327 <jd.walker327@gmail.com>
2026-03-13 14:49:36 -0500

    Replace map button with map icon link

--- app.js
+++ app.js
@@ -354,10 +354,21 @@ function renderLookupTable(rows) {
       <td>
   ${
     row.latitude && row.longitude
-      ? `<button class="map-button" data-lat="${row.latitude}" data-lon="${row.longitude}">Map</button>`
+      ? `
+        <a 
+          class="map-icon-link" 
+          href="https://www.google.com/maps?q=${row.latitude},${row.longitude}" 
+          target="_blank"
+        >
+          <svg width="20" height="20" viewBox="0 0 24 24">
+            <path d="M12 2C8.1 2 5 5.1 5 9c0 5.2 7 13 7 13s7-7.8 7-13c0-3.9-3.1-7-7-7zm0 9.5c-1.4 0-2.5-1.1-2.5-2.5S10.6 6.5 12 6.5s2.5 1.1 2.5 2.5S13.4 11.5 12 11.5z"/>
+          </svg>
+        </a>
+      `
       : ""
   }
 </td>
+
 
       <td>${escHtml(row["dot-number"] || "")}</td>
       <td>${escHtml(row.state || "")}</td>
